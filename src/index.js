0a17f5661f5d773ecfd049fd2c419c4d6a45b2a4
eman hamid <emanhharki@gmail.com>
2021-05-19 21:48:08 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('%c HI', 'color: firebrick')
+// console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+
+Fetch(imgUrl ){
+   .then(
+        response=>response.json()
+      ).then(
+        console.log(json);
+      )
+}
